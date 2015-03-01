d674e3c4f79270e0c488f15987eb2f1054579fa4
andreashuebner <andreas.huebnerh@gmail.com>
2015-03-01 11:24:31 +0000

    Status after implementation name of drink

--- bartender.py
+++ bartender.py
@@ -1,12 +1,12 @@
 import random
-questions = {
+questions = { #All questions will be asked
     "strong": "Do ye like yer drinks strong?",
     "salty": "Do ye like it with a salty tang?",
     "bitter": "Are ye a lubber who likes it bitter?",
     "sweet": "Would ye like a bit of sweetness with yer poison?",
     "fruity": "Are ye one for a fruity finish?"
 }
-ingredients = {
+ingredients = { #Possible list of ingredients, one will be chosen at random for every taste confirmed by customer
     "strong": ["glug of rum", "slug of whisky", "splash of gin"],
     "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
     "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
@@ -14,6 +14,32 @@ ingredients = {
     "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
 }
 
+listAdjectives=[] #will be populated with adjectives for naming coctails
+listNouns=[] #will be populated with nouns for naming coctials
+listAdjectives.append("fluffy")
+listAdjectives.append("sweet")
+listAdjectives.append("red")
+listAdjectives.append("Irish")
+
+listNouns.append("Martini")
+listNouns.append("Sidecar")
+listNouns.append("Rose")
+listNouns.append("Manhattan")
+
+'''Function that chooses a random adjective and noun from the list of nouns/adjectives and returns a combined name of the drink
+@return {string} - The name of the drink
+'''
+def get_name():
+    randomAdjective = random.choice(listAdjectives)
+    randomNoun = random.choice(listNouns)
+    nameOfDrink = randomAdjective + " " + randomNoun
+    return nameOfDrink
+
+
+'''Asks how customer likes his/her drinks and returns a dict with attribute of ingredient
+as key and True/False as value
+@returns {dict} Dict with attributes like "strong, sweet" a key and boolean as value
+'''
 def ask_questions():
     dictAnswers={} #Will save either True or False for the questions asked
     for key in questions:
@@ -25,7 +51,10 @@ def ask_questions():
             dictAnswers[key] = False
             
     return dictAnswers
-
+''' For every attribute in dictAnswers set to True, select a suitable ingredient at random
+@param {dict} dictAnswers - Having the attribute of ingredient as key and boolen as value
+@return {list] - Returns the list of randomly chosen suitable ingredients
+'''
 def mix_drinks(dictAnswers):
     listIngredients=[]
     for key in dictAnswers:
@@ -38,4 +67,6 @@ def mix_drinks(dictAnswers):
 
 if __name__ == "__main__":
    dictAnswers=ask_questions()
-   mix_drinks(dictAnswers)
+   mixedDrink=mix_drinks(dictAnswers)
+   nameOfDrink=get_name()
+   print ("Here is your drink " + nameOfDrink + " with the ingredients " + str(mixedDrink))
